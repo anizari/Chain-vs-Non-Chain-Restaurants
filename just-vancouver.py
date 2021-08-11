d37ac0843f37af2c2fae6f39bf653468e5a08369
Christopher Louie <cla311@sfu.ca>
2021-08-10 20:08:51 -0700

    Swich to resturants question
    Co-authored-by: anizari

--- just-vancouver.py
+++ just-vancouver.py
@@ -4,9 +4,11 @@
 # hdfs dfs -cat amenities-vancouver/* | gzip -d - | gzip -c > amenities-vancouver.json.gz
 
 import sys
+import unicodedata
 
-from PIL import Image
-from PIL.ExifTags import GPSTAGS, TAGS
+import numpy as np
+# from PIL import Image
+# from PIL.ExifTags import GPSTAGS, TAGS
 from pyspark.sql import Row, SparkSession, functions, types
 
 assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
@@ -30,7 +32,7 @@ amenity_schema = types.StructType([
 ])
 
 
-def cleanCode(poi):
+def filterData(poi):
     poi = poi.filter((poi['lon'] > -123.5) & (poi['lon'] < -122))
     poi = poi.filter((poi['lat'] > 49) & (poi['lat'] < 49.5))
 
@@ -42,113 +44,42 @@ def cleanCode(poi):
     foodArray = ['bar', 'biergarten', 'cafe', 'fast_food',
                  'food_court', 'ice_cream', 'pub', 'restaurant']
 
-    # Education
-    educationArray = ['library']
+    poi = poi.filter(poi['amenity'].isin(foodArray)).cache()
+    poi.show()
 
-    # Transportation
-    transportationArray = ['bicycle_rental', 'boat_rental', 'bus_station',
-                           'car_rental', 'charging_station', 'ferry_terminal', 'parking']
+    brands = poi.select(poi['tags']['brand']).distinct().dropna().collect()
 
-    # Financial
-    financialArray = ['atm', 'burea_de_change']
-
-    # Healthcare
-    healthcareArray = ['clinic', 'hospital', 'pharmacy']
-
-    # Entertainment, Arts, Culture
-    entertainmentArray = ['arts_centre', 'casino', 'cinema', 'community_centre',
-                          'conference_centre', 'fountain', 'nightclub', 'planetarium', 'theatre']
-
-    # Public Services
-    publicArray = ['post_box', 'post_office']
-
-    # Facilities
-    facilitiesArray = ['bench']
-
-    # Others
-    othersArray = ['clock', 'internet_cafe', 'marketplace']
-
-    poi = poi.filter(poi['amenity'].isin(foodArray)
-                     | poi['amenity'].isin(educationArray)
-                     | poi['amenity'].isin(transportationArray)
-                     | poi['amenity'].isin(financialArray)
-                     | poi['amenity'].isin(healthcareArray)
-                     | poi['amenity'].isin(entertainmentArray)
-                     | poi['amenity'].isin(publicArray)
-                     | poi['amenity'].isin(facilitiesArray)
-                     | poi['amenity'].isin(othersArray))
-
-    poi = poi.filter(~((poi['amenity'] == 'bench') & (poi['name'].isNull())))
-    poi = poi.filter(
-        ~((poi['amenity'] == 'fountain') & (poi['name'].isNull())))
+    for item in brands:
+        normal_item = unicodedata.normalize('NFKD', str(item[0]))
+        print(normal_item.encode('ASCII', 'ignore'))
 
     return poi
 
 
-def get_exif(filename):
-    # Code from https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
-    image = Image.open(filename)
-    image.verify()
-    return image._getexif()
-
-
-def get_geotagging(exif):
-    # Code from https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
-    if not exif:
-        raise ValueError("No EXIF metadata found")
-
-    geotagging = {}
-    for (idx, tag) in TAGS.items():
-        if tag == 'GPSInfo':
-            if idx not in exif:
-                raise ValueError("No EXIF geotagging found")
-
-            for (key, val) in GPSTAGS.items():
-                if key in exif[idx]:
-                    geotagging[val] = exif[idx][key]
-
-    return geotagging
-
-
-def get_decimal_from_dms(dms, ref):
-    # Code from https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
-    degrees = dms[0]
-    minutes = dms[1] / 60.0
-    seconds = dms[2] / 3600.0
-
-    if ref in ['S', 'W']:
-        degrees = -degrees
-        minutes = -minutes
-        seconds = -seconds
-
-    return round(degrees + minutes + seconds, 5)
-
-
-def get_coordinates(geotags):
-    # Code from https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
-    lat = get_decimal_from_dms(
-        geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
-
-    lon = get_decimal_from_dms(
-        geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
-
-    return (lat, lon)
+def distance(city, stations):
+    lat1 = city['latitude']
+    lon1 = city['longitude']
+    lat2 = stations['latitude']
+    lon2 = stations['longitude']
+    R = 6371
+    p = np.pi / 180  # Pi/180
+    a = 0.5 - np.cos((lat2 - lat1) * p) / 2 + np.cos(lat1 * p) * \
+        np.cos(lat2 * p) * (1 - np.cos((lon2 - lon1) * p)) / 2
+    distance = (2 * R * np.arcsin(np.sqrt(a)))
+    return distance
 
 
 def main(inputs, output):
     poi = spark.read.json(inputs, schema=amenity_schema)
-    poi = cleanCode(poi)
-
-    exif = get_exif('img/car.jpg')
-    geotags = get_geotagging(exif)
-    print(get_coordinates(geotags))
+    poi = filterData(poi)
 
     # poi.show()
-    # poi = poi.coalesce(1) # ~1MB after the filtering
+    poi = poi.coalesce(1)  # ~1MB after the filtering
     poi.write.json(output, mode='overwrite', compression='gzip')
 
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
     output = sys.argv[2]
+    # imgFolder = sys.argv[3]
     main(inputs, output)

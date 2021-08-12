ef6633019a7e60810f1eb56dbd54cd4f2495bfa7
anizari <anizari@sfu.ca>
2021-08-12 13:33:53 -0700

    Created plots for clustering classifiers.

--- just-vancouver.py
+++ just-vancouver.py
@@ -9,6 +9,8 @@ from functools import reduce
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn
 from pyspark.ml.clustering import (LDA, BisectingKMeans, GaussianMixture,
                                    KMeans, PowerIterationClustering)
 from pyspark.ml.evaluation import ClusteringEvaluator
@@ -228,8 +230,8 @@ def cluster_GMM(data, test_data):
     print("Silhouette with squared euclidean distance = " +
           str(silhouette) + str(test_silhouette))
 
-    print("Gaussians shown as a DataFrame: ")
-    model.gaussiansDF.show(truncate=False)
+    # print("Gaussians shown as a DataFrame: ")
+    # model.gaussiansDF.show(truncate=False)
 
     return predictions, test_predictions
 
@@ -241,6 +243,50 @@ def ML(data, test_data):
 
     return km_df, km_test, bkm_df, bkm_test, gmm_df, gmm_test
 
+def pandasDataframe(chain, nonchain):
+    chain_pd = chain.toPandas()
+    nonchain_pd = nonchain.toPandas()
+
+    return chain_pd, nonchain_pd
+
+def plotKMeansFig(chain, nonchain):
+    fig, (p1,p2) = plt.subplots(1,2, figsize=(16,9))
+    fig.suptitle('K-Means')
+    p1.scatter(chain.lat, chain.lon, c=chain.prediction)
+    p1.set_title('Chain Food Locations')
+    p1.set_xlabel('Latitude')
+    p1.set_ylabel('Longitude')
+    p2.scatter(nonchain.lat, nonchain.lon, c = nonchain.prediction)
+    p2.set_title('Nonchain Food Locations')
+    p2.set_xlabel('Latitude')
+    p2.set_ylabel('Longitude')
+    fig.savefig('figures/kmeans.png')
+
+def plotBkmFig(chain, nonchain):
+    fig, (p1,p2) = plt.subplots(1,2, figsize=(16,9))
+    fig.suptitle('Bisecting KMeans')
+    p1.scatter(chain.lat, chain.lon, c=chain.prediction)
+    p1.set_title('Chain Food Locations')
+    p1.set_xlabel('Latitude')
+    p1.set_ylabel('Longitude')
+    p2.scatter(nonchain.lat, nonchain.lon, c = nonchain.prediction)
+    p2.set_title('Nonchain Food Locations')
+    p2.set_xlabel('Latitude')
+    p2.set_ylabel('Longitude')
+    fig.savefig('figures/bkm.png')
+
+def plotGmmFig(chain, nonchain):
+    fig, (p1,p2) = plt.subplots(1,2, figsize=(16,9))
+    fig.suptitle('Gaussian Mixture Model')
+    p1.scatter(chain.lat, chain.lon, c=chain.prediction)
+    p1.set_title('Chain Food Locations')
+    p1.set_xlabel('Latitude')
+    p1.set_ylabel('Longitude')
+    p2.scatter(nonchain.lat, nonchain.lon, c = nonchain.prediction)
+    p2.set_title('Nonchain Food Locations')
+    p2.set_xlabel('Latitude')
+    p2.set_ylabel('Longitude')
+    fig.savefig('figures/gmm.png')
 
 def main(inputs, output):
     poi = spark.read.json(inputs, schema=amenity_schema)
@@ -250,6 +296,65 @@ def main(inputs, output):
 
     chain_km, nonchain_km, chain_bkm, nonchain_bkm, chain_gmm, nonchain_gmm = ML(
         chain, nonchain)
+
+    # chain.show()
+    # chain_km.show()
+
+    #Convert pypark dataframes to panda dataframe for plotting
+    chain_km_pd, nonchain_km_pd = pandasDataframe(chain_km, nonchain_km)
+    chain_bkm_pd, nonchain_bkm_pd = pandasDataframe(chain_bkm, nonchain_bkm)
+    chain_gmm_pd, nonchain_gmm_pd = pandasDataframe(chain_gmm, nonchain_gmm)
+
+    seaborn.set()
+
+    #Plot each clustering classifier
+
+    #KMeans
+    plotKMeansFig(chain_km_pd, nonchain_km_pd)
+
+    # fig1, (p1,p2) = plt.subplots(1,2, figsize=(16,9))
+    # fig1.suptitle('K-Means')
+    # p1.scatter(chain_km_pd.lat, chain_km_pd.lon, c=chain_km_pd.prediction)
+    # p1.set_title('Chain Food Locations')
+    # p1.set_xlabel('Latitude')
+    # p1.set_ylabel('Longitude')
+    # p2.scatter(nonchain_km_pd.lat, nonchain_km_pd.lon, c = nonchain_km_pd.prediction)
+    # p2.set_title('Nonchain Food Locations')
+    # p2.set_xlabel('Latitude')
+    # p2.set_ylabel('Longitude')
+    # fig1.savefig('figures/kmeans.png')
+
+    #BKM
+    plotBkmFig(chain_bkm_pd, nonchain_bkm_pd)
+
+    # fig2, (p3,p4) = plt.subplots(1,2, figsize=(16,9))
+    # fig2.suptitle('Bisecting KMeans')
+    # p3.scatter(chain_bkm_pd.lat, chain_bkm_pd.lon, c=chain_bkm_pd.prediction)
+    # p3.set_title('Chain Food Locations')
+    # p3.set_xlabel('Latitude')
+    # p3.set_ylabel('Longitude')
+    # p4.scatter(nonchain_bkm_pd.lat, nonchain_bkm_pd.lon, c = nonchain_bkm_pd.prediction)
+    # p4.set_title('Nonchain Food Locations')
+    # p4.set_xlabel('Latitude')
+    # p4.set_ylabel('Longitude')
+    # fig2.savefig('figures/bkm.png')
+
+    #GMM
+    plotGmmFig(chain_gmm_pd, nonchain_gmm_pd)
+
+    # fig3, (p5,p6) = plt.subplots(1,2, figsize=(16,9))
+    # fig3.suptitle('Gaussian Mixture Model')
+    # p5.scatter(chain_gmm_pd.lat, chain_gmm_pd.lon, c=chain_gmm_pd.prediction)
+    # p5.set_title('Chain Food Locations')
+    # p5.set_xlabel('Latitude')
+    # p5.set_ylabel('Longitude')
+    # p6.scatter(nonchain_gmm_pd.lat, nonchain_gmm_pd.lon, c = nonchain_gmm_pd.prediction)
+    # p6.set_title('Nonchain Food Locations')
+    # p6.set_xlabel('Latitude')
+    # p6.set_ylabel('Longitude')
+    # fig3.savefig('figures/gmm.png')
+
+
 
     # poi.show()
     poi = poi.coalesce(1)  # ~1MB after the filtering
